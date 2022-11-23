c7b0feb43d59ba2ff502e65fbe9a1dae3d61fbc3
atsupontio <akman0510@gmail.com>
2022-11-23 16:52:01 +0900

    add encode and batch proofs encode

--- src/encode.rs
+++ src/encode.rs
@@ -36,6 +36,19 @@ pub fn create_uncompressed_file(res_proof: String, res_vkey: String) -> Result<(
     Ok(())
 }
 
+pub fn create_multi_uncompressed_files(res_proof: String, res_vkey: String, count: u32) -> Result<(), io::Error> {
+
+    for i in 1..=count {
+        let mut file_uncompressed_proof = File::create(format!("{}{}{}", "./batch_file/proof", i, "_uncompressed.json")).unwrap();
+        file_uncompressed_proof.write(res_proof.as_bytes())?;
+
+        let mut file_uncompressed_vkey = File::create(format!("{}{}{}", "./batch_file/vkey", i, "_uncompressed.json")).unwrap();
+        file_uncompressed_vkey.write(res_vkey.as_bytes())?;
+    }
+
+    Ok(())
+}
+
 pub fn encode_hex(bytes: &[u8]) -> String {
     let mut s = String::with_capacity(bytes.len() * 2);
     for &b in bytes {
@@ -82,18 +95,6 @@ pub fn encode_uncompressed() -> Result<(), io::Error> {
     let res_vd2 = format!("{}{}", "0x", encode_hex(&vkey_d2));
     let res_vic1 = format!("{}{}", "0x", encode_hex(&vkey_ic_1));
     let res_vic2 = format!("{}{}", "0x", encode_hex(&vkey_ic_2));
-    println!("pi_a: {}", res_a);
-    println!("pi_b: {}", res_b);
-    println!("pi_c: {}", res_c);
-    println!("vkey_alpha: {}", res_va);
-    println!("vkey_beta1: {}", res_vb1);
-    println!("vkey_beta2: {}", res_vb2);
-    println!("vkey_gamma: {}", res_vg);
-    println!("vkey_delta1: {}", res_vd1);
-    println!("vkey_delta2: {}", res_vd2);
-    println!("vkey_ic_1: {}", res_vic1);
-    println!("vkey_ic_2: {}", res_vic2);
-
 
     let mut file_proofa = File::create("./file/proof_a.txt").unwrap();
     file_proofa.write(res_a.as_bytes())?;
@@ -118,6 +119,76 @@ pub fn encode_uncompressed() -> Result<(), io::Error> {
     file_vkey_ic_1.write(res_vic1.as_bytes())?;
     let mut file_vkey_ic_2 = File::create("./file/vkey_ic_2.txt").unwrap();
     file_vkey_ic_2.write(res_vic2.as_bytes())?;
+
+    Ok(())
+}
+
+
+pub fn encode_multi_uncompressed(count: u32, max_count: u32) -> Result<(), io::Error> {
+
+    let proof_file = File::open(format!("{}{}{}", "./batch_file/proof", count, "_uncompressed.json")).unwrap();
+
+    // let file = OpenOptions::new().read(true);
+
+    let proof_reader = BufReader::new(proof_file);
+
+    let deserialized_proof: Proof = serde_json::from_reader(proof_reader).unwrap();
+
+    let pi_a = deserialized_proof.pi_a;
+    let pi_b = deserialized_proof.pi_b;
+    let pi_c = deserialized_proof.pi_c;
+    
+    let res_a = format!("{}{}", "0x", encode_hex(&pi_a));
+    let res_b = format!("{}{}", "0x", encode_hex(&pi_b));
+    let res_c = format!("{}{}", "0x", encode_hex(&pi_c));
+
+    let mut file_proofa = File::create(format!("{}{}{}", "./batch_file/proof", count, "_a.txt")).unwrap();
+    file_proofa.write(res_a.as_bytes())?;
+    let mut file_proofb = File::create(format!("{}{}{}", "./batch_file/proof", count, "_b.txt")).unwrap();
+    file_proofb.write(res_b.as_bytes())?;
+    let mut file_proofc = File::create(format!("{}{}{}", "./batch_file/proof", count, "_c.txt")).unwrap();
+    file_proofc.write(res_c.as_bytes())?;
+
+    if count == max_count {
+        let vkey_file = File::open(format!("{}{}{}", "./batch_file/vkey", count, "_uncompressed.json")).unwrap();
+        let vkey_reader = BufReader::new(vkey_file);
+        let deserialized_vkey: VerifyKey = serde_json::from_reader(vkey_reader).unwrap();
+
+        let vkey_a = deserialized_vkey.alpha_1;
+        let vkey_b1 = deserialized_vkey.beta_1;
+        let vkey_b2 = deserialized_vkey.beta_2;
+        let vkey_g = deserialized_vkey.gamma_2;
+        let vkey_d1 = deserialized_vkey.delta_1;
+        let vkey_d2 = deserialized_vkey.delta_2;
+        let vkey_ic_1 = &deserialized_vkey.ic[0];
+        let vkey_ic_2 = &deserialized_vkey.ic[1];
+    
+        let res_va = format!("{}{}", "0x", encode_hex(&vkey_a));
+        let res_vb1 = format!("{}{}", "0x", encode_hex(&vkey_b1));
+        let res_vb2 = format!("{}{}", "0x", encode_hex(&vkey_b2));
+        let res_vg = format!("{}{}", "0x", encode_hex(&vkey_g));
+        let res_vd1 = format!("{}{}", "0x", encode_hex(&vkey_d1));
+        let res_vd2 = format!("{}{}", "0x", encode_hex(&vkey_d2));
+        let res_vic1 = format!("{}{}", "0x", encode_hex(&vkey_ic_1));
+        let res_vic2 = format!("{}{}", "0x", encode_hex(&vkey_ic_2));
+
+        let mut file_vkey_a = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_a.txt")).unwrap();
+        file_vkey_a.write(res_va.as_bytes())?;
+        let mut file_vkey_b1 = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_b1.txt")).unwrap();
+        file_vkey_b1.write(res_vb1.as_bytes())?;
+        let mut file_vkey_b2 = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_b2.txt")).unwrap();
+        file_vkey_b2.write(res_vb2.as_bytes())?;
+        let mut file_vkey_g = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_g.txt")).unwrap();
+        file_vkey_g.write(res_vg.as_bytes())?;
+        let mut file_vkey_d1 = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_d1.txt")).unwrap();
+        file_vkey_d1.write(res_vd1.as_bytes())?;
+        let mut file_vkey_d2 = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_d2.txt")).unwrap();
+        file_vkey_d2.write(res_vd2.as_bytes())?;
+        let mut file_vkey_ic_1 = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_ic_1.txt")).unwrap();
+        file_vkey_ic_1.write(res_vic1.as_bytes())?;
+        let mut file_vkey_ic_2 = File::create(format!("{}{}{}", "./batch_file/vkey", count, "_ic_2.txt")).unwrap();
+        file_vkey_ic_2.write(res_vic2.as_bytes())?;
+    }
 
     Ok(())
 
